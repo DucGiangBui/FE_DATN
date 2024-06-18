a387c5b45d489bafd7df9f6d4f7c0a8dd9b30eed
Đức Giang <>
2024-06-18 17:57:16 +0700

    no message

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,8 +30,34 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener("DOMContentLoaded", function() {
     const buyButton = document.getElementById('buy-header');
     const introduceSection = document.querySelector('.introduction-5');
+    const navLinks = document.querySelectorAll(".btn-header-1");
+
 
     buyButton.addEventListener('click', function() {
         introduceSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".btn-header-1");
+
+    window.addEventListener("scroll", function() {
+        let current = "";
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - 60; // Adjust this value based on your header height
+            const sectionHeight = section.clientHeight;
+            if (window.scrollY >= sectionTop && window.scrollY <= sectionTop + sectionHeight) {
+                current = section.getAttribute("id") || section.classList[0];
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove("active");
+            if (link.getAttribute("href").substring(1) === current) {
+                link.classList.add("active");
+            }
+        });
     });
 });
